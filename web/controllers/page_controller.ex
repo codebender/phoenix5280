4de19a6551a452fa0d7657f8477865da74b2f2b1
Matthew Bender <benderm@gmail.com>
2016-08-21 22:58:40 -0600

    add some 1st round of styling

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,7 @@ defmodule Phoenix5280.PageController do
   use Phoenix5280.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    {:ok, blog_posts} = Phoenix5280.Blog.list()
+    render conn, "index.html", blog_posts: blog_posts
   end
 end
